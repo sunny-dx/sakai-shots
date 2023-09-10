0b941a35dc70ccb1d73de94e6964be7a3ae6b61a
Sunny <sunny-dx@users.noreply.github.com>
2023-09-10 23:36:10 +0530

    removed skia

--- src/Preview.js
+++ src/Preview.js
@@ -1,69 +1,42 @@
-import React, { useEffect, useState } from "react";
-import {
-  Image,
-  ImageBackground,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { viewportHeight, viewportWidth } from "../constants";
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-import { useGlobalContext, usePreviewRef } from "../GlobalContext";
-import ImagePicker from "./ImagePicker";
-
-const HelloWorld = () => {
-  const size = 256;
-  const r = size * 0.33;
-  return (
-    <Canvas style={{ flex: 1 }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={size - r} cy={r} r={r} color="magenta" />
-        <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
-      </Group>
-    </Canvas>
-  );
-};
-
-const Preview = ({ scale, exportable }) => {
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import {viewportHeight, viewportWidth} from '../constants';
+import {useGlobalContext, usePreviewRef} from '../GlobalContext';
+import ImagePicker from './ImagePicker';
+const Preview = ({scale, exportable}) => {
   const [state, dispatch] = useGlobalContext();
   const previewRef = usePreviewRef();
   return (
     <View
       style={{
         height: viewportHeight * scale - 200,
-        // backgroundColor: "red",
         width: viewportWidth * scale,
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-      }}
-    >
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+      }}>
       <View
         style={{
           borderRadius: 10,
-          overflow: "hidden",
+          overflow: 'hidden',
           borderWidth: 2,
-          borderColor: "rgba(255, 255, 255, 0.1)",
-        }}
-      >
+          borderColor: 'rgba(255, 255, 255, 0.1)',
+        }}>
         <View
           style={{
             width: viewportWidth * scale - 30,
             minHeight: 100,
-            backgroundColor: "grey",
+            backgroundColor: 'grey',
             aspectRatio: state.aspectRatio,
             padding: state.padding * 50 * scale,
-            alignItems: "center",
-            justifyContent: "center",
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
-          {...(exportable ? { ref: previewRef } : {})}
-        >
+          {...(exportable ? {ref: previewRef} : {})}>
           <state.background width={viewportWidth * scale} />
           <ImagePicker scale={scale} />
         </View>
       </View>
-      {/* <ImageMenu /> */}
     </View>
   );
 };
